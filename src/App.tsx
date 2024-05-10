4f0fb352e2efefea2094cf5d937013f90225b439
bmancilla23 <bmancilla119@gmail.com>
2024-05-10 12:38:05 -0500

    feat: update imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
-export const App = () => {
-	return <div>App</div>;
+import { FC } from "react";
+import { Header } from "./common/header/Header";
+import { Cards } from "./components/main";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useThemeContext } from "./theme/ThemeContextProvider";
+import { Charts } from "./components/main/charts/Charts";
+import { Users } from "./components/main/users/Users";
+import { TableData } from "./components/main/tableData/TableData";
+import { Footer } from "./common/footer/Footer";
+
+export const App: FC = () => {
+
+  const { theme } = useThemeContext()
+
+  return (
+
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Cards />
+      <Charts />
+      <Users />
+      <TableData />
+      <Footer />
+    </ThemeProvider>
+  )
 };
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import './index.css';
+import './Global.css';
+import { ThemeContextProvider } from './theme/ThemeContextProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+
+  <React.StrictMode>
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
+  </React.StrictMode>
 );
